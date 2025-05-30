9560424f2b4223f280c1a67637530029c0cf72c1
ota32 <otacorporation0520@gmail.com>
2025-05-30 11:29:13 +0900

    public フォルダを移動

--- app/routes/admin/admin-layout.tsx
+++ app/routes/admin/admin-layout.tsx
@@ -1,10 +1,13 @@
 import {Outlet} from 'react-router'
+import {SidebarComponent} from '@syncfusion/ej2-react-navigations'
 
 const AdminLayout = () => {
   return (
     <div className="admin-layout">
       MobileSidebar
-      <aside className="w-full max-w-[270px] hidden lg:block">Sidebar</aside>
+      <aside className="w-full max-w-[270px] hidden lg:block">
+        Sidebar
+      </aside>
       
     <aside className="children">
         <Outlet />
