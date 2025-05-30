debdc6d804103285aac170912ac2758d98309d52
ota32 <otacorporation0520@gmail.com>
2025-05-30 17:30:32 +0900

    モバイルメニュー追加完了

--- app/routes.ts
+++ app/routes.ts
@@ -3,6 +3,7 @@ import { type RouteConfig, route, layout } from "@react-router/dev/routes";
 export default [
   layout("routes/admin/admin-layout.tsx", [
     route("dashboard", "routes/admin/dashboard.tsx"),
+    route("all-users", "routes/admin/all-users.tsx"),
     route("okeyan", "routes/admin/okeyan.tsx"),
     route("kaneyan", "routes/admin/kaneyan.tsx"),
     route("hanesan", "routes/admin/hanesan.tsx"),
--- app/routes/admin/admin-layout.tsx
+++ app/routes/admin/admin-layout.tsx
@@ -1,19 +1,21 @@
-import {Outlet} from 'react-router'
-import {SidebarComponent} from '@syncfusion/ej2-react-navigations'
+import { Outlet } from "react-router";
+import { SidebarComponent } from "@syncfusion/ej2-react-navigations";
+import { MobileSidebar, NavItems } from "../../../components";
 
 const AdminLayout = () => {
   return (
     <div className="admin-layout">
-      MobileSidebar
+      <MobileSidebar/>
       <aside className="w-full max-w-[270px] hidden lg:block">
-        Sidebar
+        <SidebarComponent id="sidebar" width="270px" enableGestures={false}>
+          <NavItems />
+        </SidebarComponent>
       </aside>
-      
-    <aside className="children">
+      <aside className="children">
         <Outlet />
       </aside>
-      </div>
-  )
-}
+    </div>
+  );
+};
 
-export default AdminLayout
+export default AdminLayout;
